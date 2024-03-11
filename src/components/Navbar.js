8b4a2b1f742add19d17355b70126575ba37505e3
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-03-11 13:56:03 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand mx-3 mx-lg-5">Zealous Bank</a>
+        <a className="navbar-brand mx-3 mx-lg-5">MR KIKSY WEBSITE DEVELOPMENT CLASS</a>
 
         <form className="d-flex">
           <Link
