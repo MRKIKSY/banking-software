b1b460bb5b2bf29b7b35c416c79eb1340e4fcc8f
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-03-12 02:32:40 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
+
 export default function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand mx-3 mx-lg-5">MR KIKSY WEBSITE DEVELOPMENT CLASS</a>
-
+        <a className="navbar-brand mx-3 mx-lg-5">MR KIKSY WEBSITE DESIGN CLASS</a>
+      
+       
         <form className="d-flex">
           <Link
             className="btn btn-outline-light mx-3 mx-lg-5"
