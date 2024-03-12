e1b577ad841cee5f5d93d363b37de6eb95ed2f42
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-03-12 02:35:39 +0000

    Update MainLayout.js

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -3,18 +3,29 @@ import ViewTransactions from "./components/ViewTransactions";
 import UserPage from "./components/UserPage";
 import { BrowserRouter as Router, Rou, Route, Routes } from "react-router-dom";
 
+import LessonPage from "./components/LessonPage";
+
+
 function MainLayout() {
+
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path="/account/:index" element={<UserPage />} />
+        <Route exact
+         path ="/account/:index"
+        element={<UserPage />} />
         <Route
           exact
           path="/transactions/:index"
           element={<ViewTransactions />}
         />
+       
+        <Route path ="/lessonpage/:index" element={<LessonPage />}/>
+         
       </Routes>
+      
     </div>
   );
 }
